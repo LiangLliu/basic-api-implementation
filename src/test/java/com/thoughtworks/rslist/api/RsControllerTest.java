5d2e423475390e8f135642581717ef0e64c9a016
Liangliang Liu <max1141847753@gmail.com>
2020-08-04 18:09:43 +0800

    [Liangliang Liu] 使用 Nested 方法将所有的get请求写到一起

--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.rslist.api;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,29 +20,37 @@ class RsControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void should_get_rs_list_when_given_get_request() throws Exception {
-        getMethodTest("/rs/list", "[第一条事件, 第二条事件, 第三条事件]");
+    /**
+     * @Nested 使用内部类的形式将同类型的测试放在一起
+     * GetMethodTest : 测试所有的get请求
+     */
+    @Nested
+    public class GetRequestTest {
+        @Test
+        public void should_get_rs_list_when_given_get_request() throws Exception {
+            getMethodTest("/rs/list", "[第一条事件, 第二条事件, 第三条事件]");
+        }
+
+        @Test
+        public void should_get_one_rs_event_when_given_event_id() throws Exception {
+            getMethodTest("/rs/1", "第一条事件");
+            getMethodTest("/rs/2", "第二条事件");
+            getMethodTest("/rs/3", "第三条事件");
+        }
+
+        @Test
+        public void should_get_a_list_of_ranges_when_given_between_two_index() throws Exception {
+            getMethodTest("/rs/list?start=1&end=2", "[第一条事件, 第二条事件]");
+            getMethodTest("/rs/list?start=2&end=3", "[第二条事件, 第三条事件]");
+            getMethodTest("/rs/list?start=1&end=3", "[第一条事件, 第二条事件, 第三条事件]");
+        }
+
+        private void getMethodTest(String url, String result) throws Exception {
+            mockMvc.perform(get(url))
+                    .andExpect(content().string(result))
+                    .andExpect(status().isOk());
+        }
     }
 
-    @Test
-    public void should_get_one_rs_event_when_given_event_id() throws Exception {
-        getMethodTest("/rs/1", "第一条事件");
-        getMethodTest("/rs/2", "第二条事件");
-        getMethodTest("/rs/3", "第三条事件");
-    }
-
-    @Test
-    public void should_get_a_list_of_ranges_when_given_between_two_index() throws Exception {
-        getMethodTest("/rs/list?start=1&end=2", "[第一条事件, 第二条事件]");
-        getMethodTest("/rs/list?start=2&end=3", "[第二条事件, 第三条事件]");
-        getMethodTest("/rs/list?start=1&end=3", "[第一条事件, 第二条事件, 第三条事件]");
-    }
-
-    private void getMethodTest(String url, String result) throws Exception {
-        mockMvc.perform(get(url))
-                .andExpect(content().string(result))
-                .andExpect(status().isOk());
-    }
 
 }
